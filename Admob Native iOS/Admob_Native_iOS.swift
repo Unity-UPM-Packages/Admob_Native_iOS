3d44147cfa4eedec8c16269b952252f5eafbb276
QuangHaiVu <haivq@bfungame.net>
2026-08-25 13:38:36 +0700

    controller + layout

--- Admob Native iOS/Admob_Native_iOS.swift
+++ Admob Native iOS/Admob_Native_iOS.swift
@@ -2,8 +2,14 @@
 //  Admob_Native_iOS.swift
 //  Admob Native iOS
 //
-//  Created by MacBF on 25/8/26.
+//  Framework entrypoint and module exports for AdmobNativeiOS.
 //
 
 import Foundation
+import UIKit
+import GoogleMobileAds
 
+public final class AdmobNativeiOS {
+    public static let version = "1.0.0"
+    public static let supportedAdmobSdkVersion = "11.13.0"
+}
