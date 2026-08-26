b3d921ecb9cb0e420484f6e1436f61023fcca618
QuangHaiVu <haivq@bfungame.net>
2026-08-26 16:13:59 +0700

    build script

--- build_xcframework.sh
+++ build_xcframework.sh
@@ -9,8 +9,9 @@ set -e
 SCHEME_NAME="Admob Native iOS"
 PROJECT_NAME="Admob Native iOS.xcodeproj"
 FRAMEWORK_NAME="Admob_Native_iOS"
+OUTPUT_FRAMEWORK_NAME="admob_native_unity"
 OUTPUT_DIR="./build"
-XCFRAMEWORK_DIR="${OUTPUT_DIR}/${FRAMEWORK_NAME}.xcframework"
+XCFRAMEWORK_DIR="${OUTPUT_DIR}/${OUTPUT_FRAMEWORK_NAME}.xcframework"
 
 echo "🚀 [1/4] Dọn dẹp thư mục build cũ..."
 rm -rf "${OUTPUT_DIR}"
@@ -34,7 +35,7 @@ xcodebuild archive \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-echo "📦 [4/4] Đang tạo universal ${FRAMEWORK_NAME}.xcframework..."
+echo "📦 [4/4] Đang tạo universal ${OUTPUT_FRAMEWORK_NAME}.xcframework..."
 xcodebuild -create-xcframework \
     -framework "${OUTPUT_DIR}/ios_devices.xcarchive/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework" \
     -framework "${OUTPUT_DIR}/ios_simulators.xcarchive/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework" \
@@ -44,5 +45,5 @@ echo ""
 echo "=============================================================================="
 echo "✅ XUẤT XCFRAMEWORK THÀNH CÔNG!"
 echo "📍 Đường dẫn: ${XCFRAMEWORK_DIR}"
-echo "👉 File được lưu tại thư mục: ${OUTPUT_DIR}/${FRAMEWORK_NAME}.xcframework"
+echo "👉 File được lưu tại thư mục: ${OUTPUT_DIR}/${OUTPUT_FRAMEWORK_NAME}.xcframework"
 echo "=============================================================================="
